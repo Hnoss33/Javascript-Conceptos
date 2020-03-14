916ca09f71296666a230fa6a4f8c7f3aab8e9ab2
Hnoss33 <52913599+Hnoss33@users.noreply.github.com>
2020-03-14 15:35:15 +1100

    operadores

---
+++ Operaciones.js
@@ -0,0 +1,31 @@
+//OPERADORES
+//Operadores Binarios:
+3 + 2;
+50 - 10;
+10 * 20;
+20 / 2;
+//Tambien sirve para concatenar String:
+"Diego " + "De Granda";
+
+//Operador de negación:
+!true; //devolverá false
+!false; //devolverá true
+
+//Operador de asignación:
+var a = 3;
+
+//operador de comparación:
+3 == "3"; // va comparar el valor mas no el tipo
+3 === "3"; // compara que el valor y el tipo sean iguales
+5 > 3; //5 es mayor que 3 es true
+5 < 3; // 5 es menor que 3 es false
+5 >= 6; // 5 es mayor o igual 6 es false
+5 <= 6; // 5 es menor o igual que 6 es true
+
+a && b; // va validar que a y b son verdad se cumple esa condición
+a || b; // va validar que solo a o b sea verdad para que se cumpla esa condición mas flexible
+
+//operadores de incremento:
+var edad = 40;
+edad++; //el valor de edad se va incrementar en uno.
+edad += 2; // el valor de edad va incrementar en dos.
